b48e241b004abe5e258efe02ebdd4beb00463b29
Hassan El-Shafei <73042745+HSN-Elshafei@users.noreply.github.com>
2023-04-02 02:50:46 +0200

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,37 @@
+import 'dart:ui';   
+import 'package:flutter/material.dart'; 
+  
+ void main() { 
+   runApp(const MyApp()); 
+ } 
+  
+ class MyApp extends StatelessWidget { 
+   const MyApp({super.key}); 
+  
+   // This widget is the root of your application. 
+   @override 
+   Widget build(BuildContext context) { 
+     return MaterialApp( 
+       debugShowCheckedModeBanner: false, 
+       home: Scaffold( 
+         appBar: AppBar( 
+           title: const Text("Birthday Card"), 
+         ), 
+         body: Column( 
+           children: [ 
+             Padding( 
+               padding: const EdgeInsets.only(top: 60), 
+               child: Image.asset( 
+                 "Birthday.jpg", 
+               ), 
+             ), 
+             const Text( 
+               "Happy Birthday !!", 
+               style: TextStyle(fontSize: 25), 
+             ) 
+           ], 
+         ), 
+       ), 
+     ); 
+   } 
+ }
